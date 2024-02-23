70e57e17a0cdae7c48e88f44f5b03df7cee70f20
lenss24 <rizkirodiansah3@gmail.com>
2024-02-24 00:46:57 +0700

    y

---
+++ resources/views/layouts/frontend/footer.blade.php
@@ -0,0 +1,32 @@
+<footer class="footer-area">
+    <!-- Top Footer Area -->
+    <div class="top-footer-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <!-- Footer Logo -->
+                    <div class="footer-logo">
+                        <a href="/"><h3 class="text-light">PKBM Pelita Mekarsari</h3></a>
+                    </div>
+                    <!-- Copywrite -->
+                    <p><a href="#"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright Valentino Yusuf Iskandar &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Bottom Footer Area -->
+    <div class="bottom-footer-area d-flex justify-content-between align-items-center">
+        <!-- Contact Info -->
+        <div class="contact-info">
+            <a href="#"><span>Phone:</span> 0813-8081-2727</a>
+            <a href="#"><span>Email:</span> masiskandar78@gmail.com</a>
+        </div>
+        <!-- Follow Us -->
+        <div class="follow-us">
+            <a href="{{ route('login') }}"><i class="fa fa-sign-in" aria-hidden="true"></i></a>
+            <a href="https://www.facebook.com/iskandar.adinata.75"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+        </div>
+    </div>
+</footer>
